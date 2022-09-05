49257be9f19097af101aafbd9b64d34a779b4ff2
WingsOfDesire404 <ScandinaviaAurora@proton.me>
2022-09-05 12:04:03 +0800

    Lab1 Exercise2

---
+++ HW0/Max.java
@@ -0,0 +1,19 @@
+
+public class Max
+{
+	public static int max(int[] m)
+	{
+		int max =  m[0];
+		for (int i = 1; i < m.length -1;  i++)
+		{
+			if (max <= m[i] )
+				max = m[i];
+		}
+		return max;
+	}
+	public static void main(String args[])
+	{
+		int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6};
+		System.out.println(max(numbers));
+	}
+}
